c2b3aba9482092b9c8aebb11c526726cc07c3e9d
andikscript <andikscript@gmail.com>
2022-07-19 15:13:55 +0700

    add RuntimeException handler

--- src/main/java/com/andikscript/fileupload/exception/FileUploadExceptionAdvice.java
+++ src/main/java/com/andikscript/fileupload/exception/FileUploadExceptionAdvice.java
@@ -1,28 +1,35 @@
 package com.andikscript.fileupload.exception;
 
 import com.andikscript.fileupload.message.ResponseMessage;
-import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(SizeLimitExceededException.class)
-    public ResponseEntity<ResponseMessage> handleMaxFileSize(SizeLimitExceededException exc) {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ResponseMessage> handleMaxFileSize(MaxUploadSizeExceededException exc) {
         return ResponseEntity
                 .status(HttpStatus.EXPECTATION_FAILED)
                 .body(new ResponseMessage("File to large"));
     }
 
     @ExceptionHandler(MultipartException.class)
-    public ResponseEntity<ResponseMessage> fileNotFound(MultipartException exc) {
+    public ResponseEntity<ResponseMessage> paramsNotFound(MultipartException exc) {
         return ResponseEntity
                 .status(HttpStatus.EXPECTATION_FAILED)
                 .body(new ResponseMessage("File not found"));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResponseMessage> fileIsEmpty(RuntimeException exc) {
+        return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseMessage("File is Empty"));
     }
 }
