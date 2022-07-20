a1dfcf4b9ce3d234b152d9b3c9a9807762322c39
andikscript <andikscript@gmail.com>
2022-07-20 16:10:38 +0700

    add enum for classpath store

---
+++ src/main/java/com/andikscript/fileupload/enums/PathStore.java
@@ -0,0 +1,11 @@
+package com.andikscript.fileupload.enums;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PathStore {
+
+    public final static String ADMIN = "uploads/admin";
+
+    public final static String USER = "uploads/user";
+}
